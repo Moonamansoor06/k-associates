395549f9360ff2fe1fa15db3d66bed6c34c47812
Moonamansoor06 <moonamansoor06@gmail.com>
2022-02-26 00:03:40 +0500

    code editted

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     'gatsby-plugin-theme-ui',
+    'gatsby-plugin-emotion',
 
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
         return (
 
           <Layout>
-            <Header />
+     
             
             <HomeComponent data={data} />
 
