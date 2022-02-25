c3b6d882ed737ff645d1f5a660392750c453fbed
Moonamansoor06 <moonamansoor06@gmail.com>
2022-02-25 16:11:33 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import  React from "react"
 import { StaticQuery, graphql } from 'gatsby';
 import HomeComponent from '../components/HomeComponent'
 import Layout from "../components/layout";
