788ef618f1574e3eda4d3b043092f7f587093859
Moonamansoor06 <moonamansoor06@gmail.com>
2022-02-25 12:24:52 +0500

    package-lock changed

---
+++ recipe-master/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import {Link} from 'gatsby'
+import Layout from '../components/Layout'
+
+
+
+
+export default function home() {
+    return (
+        <Layout>
+
+            <h1>This is my home page</h1>
+            
+        </Layout>
+    )
+}
