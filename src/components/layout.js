524f24d54dedf2557a659ab2e429d20c93672df5
Moonamansoor06 <moonamansoor06@gmail.com>
2022-02-27 12:33:52 +0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import NavBar from "./NavBar"
 import "./layout.css"
 import NavBar2 from "./NavBar2"
